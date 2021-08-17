49d55fabec6eb81ad3c9335311763cc5de6a487a
Jorien Caron <jorien_caron@hotmail.com>
2021-08-17 17:53:46 +0200

    finished lab

---
+++ lab_sql_intro_finished.sql
@@ -0,0 +1,22 @@
+SELECT *
+FROM actor;
+SELECT *
+from film;
+SELECT *
+from film_actor;
+SELECT *
+from category;
+-- also clicked on the 'i' of the tables to explore the databases
+SELECT title
+from FILM;
+SELECT name as 'language'
+from language;
+SELECT count(store_id)
+from store;
+-- there company has two stores
+SELECT count(staff_id)
+from staff;
+-- the company has two employees
+SELECT first_name
+from staff;
+-- The first names of the employees are Mike and Jon
